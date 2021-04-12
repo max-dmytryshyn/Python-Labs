eeddbfa1443d9b01296e896654bfbec422390d5c
max-dmytryshyn <maksumus22@gmail.com>
2021-04-12 10:46:52 +0300

    Added marshmallow schema deserialization instead of deserialization field by field, Added throwing 404 Not Found if object doesn't exist(wrong id) instead of throwing 500, Removed unnecessary method serialize in saw model, Added method update, which updates fields, to saw model

--- crud.py
+++ crud.py
@@ -1,11 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import mysql.connector
-import os
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://flask_tutorial:flask01.04.2021@localhost:3306/lab-6'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -26,14 +23,15 @@ class Saw(db.Model):
         self.material_to_saw = material_to_saw
         self.drive_type = drive_type
 
-    def serialize(self):
-        return {
-                "made_of_material": self.made_of_material,
-                "username": self.username,
-                "length_in_cm": self.length_in_cm,
-                "material_to_saw": self.material_to_saw,
-                "drive_type": self.drive_type
-                }
+    def update(self, made_of_material, username, length_in_cm, material_to_saw, drive_type):
+        self.__init__(made_of_material, username, length_in_cm, material_to_saw, drive_type)
+
+
+def get_saw_by_id(id):
+    saw = Saw.query.get(id)
+    if saw is None:
+        return abort(404)
+    return saw
 
 
 class SawSchema(ma.Schema):
@@ -47,21 +45,15 @@ saws_schema = SawSchema(many=True)
 
 @app.route("/saw", methods=["POST"])
 def add_saw():
-    made_of_material = request.json['made_of_material']
-    username = request.json['username']
-    length_in_cm = request.json['length_in_cm']
-    material_to_saw = request.json['material_to_saw']
-    drive_type = request.json['drive_type']
-
-    new_saw = Saw(made_of_material, username, length_in_cm, material_to_saw, drive_type)
+    fields = saw_schema.load(request.json)
+    new_saw = Saw(**fields)
 
     db.session.add(new_saw)
     db.session.commit()
 
-    return jsonify(new_saw.serialize())
+    return saw_schema.jsonify(new_saw)
 
 
-# endpoint to show all users
 @app.route("/saw", methods=["GET"])
 def get_saw():
     all_saws = Saw.query.all()
@@ -71,24 +63,15 @@ def get_saw():
 
 @app.route("/saw/<id>", methods=["GET"])
 def saw_detail(id):
-    saw = Saw.query.get(id)
+    saw = get_saw_by_id(id)
     return saw_schema.jsonify(saw)
 
 
 @app.route("/saw/<id>", methods=["PUT"])
 def saw_update(id):
-    saw = Saw.query.get(id)
-    made_of_material = request.json['made_of_material']
-    username = request.json['username']
-    length_in_cm = request.json['length_in_cm']
-    material_to_saw = request.json['material_to_saw']
-    drive_type = request.json['drive_type']
-
-    saw.made_of_material = made_of_material
-    saw.username = username
-    saw.length_in_cm = length_in_cm
-    saw.material_to_saw = material_to_saw
-    saw.drive_type = drive_type
+    saw = get_saw_by_id(id)
+    fields = saw_schema.load(request.json)
+    saw.update(**fields)
 
     db.session.commit()
     return saw_schema.jsonify(saw)
@@ -96,7 +79,7 @@ def saw_update(id):
 
 @app.route("/saw/<id>", methods=["DELETE"])
 def saw_delete(id):
-    saw = Saw.query.get(id)
+    saw = get_saw_by_id(id)
     db.session.delete(saw)
     db.session.commit()
 
